b1339730ad91f1b901de4161a1df3287334ca0d5
MagaBenG <ben--g2@hotmail.com>
2014-07-11 19:42:16 -0400

    Beginning of Swift Fall

---
+++ Swift Fall/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Swift Fall
+//
+//  Created by Benjamin DosSantos on 7/11/14.
+//  Copyright (c) 2014 BlackPenSoftware. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
